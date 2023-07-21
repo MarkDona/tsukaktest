d22f045eaa339ff8df9dca128d3fddc4a75d88fe
DONATUS ANKAH <ankahdonatus@gmail.com>
2023-07-21 14:07:36 +0000

    Update generate-token.js

--- js/generate-token.js
+++ js/generate-token.js
@@ -70,53 +70,7 @@ function getCandidateData() {
     var agentID = "";
     var newTokenKey = "";
 
-    function saveTokenToFirebase(token, candidateName, candidateEmail) {
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          // User is signed in.
-          agentID = user.uid;
-          var timestamp = new Date().toJSON();
-          console.log("AgentID: " + agentID);
-
-          var tokenData = {
-          token: token,
-          candidateName: candidateName,
-          candidateEmail: candidateEmail,
-          linkWithToken: ` https://tsuks-marvelous-project.webflow.io/application-landing-page`,
-          linkStatus: 'Copied',
-          tokenStatus: 'Active',
-          createdAt: timestamp,
-          linkAccessedAt: '',
-          formSubmittedAt: ''
-        };
-
-        newTokenKey = firebase.database().ref().child('tokens').push().key;
-        var updates = {};
-        updates['/tokens/' + newTokenKey] = tokenData;
-        firebase.database().ref('agents/' + agentID).update(updates);
-        console.log("New Key:" + newTokenKey);
-
-        generateLink(newTokenKey);
-        return newTokenKey;
-        } 
-      });
-    }
-
-    onfocus = function() {
-      location.reload(true);
-    };
-
-    function showPopup() {
-        var popupContainer = document.querySelector(".popup-container");
-      popupContainer.style.display = "block";
-    }
-
-    function closePopup() {
-      var popupContainer = document.querySelector(".popup-container");
-      popupContainer.style.display = "none";
-    }
-
-    function generateLink(newTokenKey) {
+   function generateLink(newTokenKey) {
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           // User is signed in.
@@ -147,6 +101,22 @@ function getCandidateData() {
         
       });
     }
+
+    onfocus = function() {
+      location.reload(true);
+    };
+
+    function showPopup() {
+        var popupContainer = document.querySelector(".popup-container");
+      popupContainer.style.display = "block";
+    }
+
+    function closePopup() {
+      var popupContainer = document.querySelector(".popup-container");
+      popupContainer.style.display = "none";
+    }
+
+    
 
     function copyLink() {
       var linkElement = document.getElementById("link");
