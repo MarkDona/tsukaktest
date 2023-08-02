ec877ecc8220a55d0049ee411d30c49165170419
DONATUS ANKAH <ankahdonatus@gmail.com>
2023-08-02 15:08:56 +0000

    Update firebase-auth.js

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -96,10 +96,11 @@ function signup() {
               data: { content: name },
               success: function(response) {
                 console.log('Email sent successfully:', response);
-                // window.location.href = "agent-login";
+                window.location.href = "agent-login";
               },
               error: function(xhr, status, error) {
                 console.error('Error sending email:', error);
+                window.location.href = "agent-login";
               }
             });
           } else {
